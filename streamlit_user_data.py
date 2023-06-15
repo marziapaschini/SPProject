64eceeca7126589806caa3d97dc818c2be5ccc0f
marziapaschini <100375901+marziapaschini@users.noreply.github.com>
2023-06-15 15:24:51 +0200

    Update streamlit_user_data.py

--- streamlit_user_data.py
+++ streamlit_user_data.py
@@ -1,14 +1,20 @@
 import streamlit as st
+import model as md
 
-def insert_user_data():
-    option = st.selectbox('Seleziona la posizione:', ['Milano', 'Roma', 'Napoli', 'Firenze'])
-    temp = st.text_input('Inserisci la temperatura:', key='temp_input')
-    if temp.isdigit():
-        temp = round(float(temp), 1)
-        st.write("Posizione:", option)
-        st.write("Temperatura:", temp)
-    else:
-        st.error("Per favore inserisci una temperatura valida (un numero con una cifra decimale).")
+def run():
+    st.write("# Weather forecast")
 
-st.write("### Insert data")
-insert_user_data()
+    # user parameters insertion
+    MinTemp = st.number_input("Enter today's minimum temperature (°C):", format="%0.1f", help="Temperature in degrees Celsius")
+    MaxTemp = st.number_input("Enter today's maximum temperature (°C):", format="%0.1f", help="Temperature in degrees Celsius")
+    Rainfall = st.number_input("Enter the amount of rain today (mm):", format="%0.1f", help="Rainfall amount in millimeters")
+    Humidity = st.number_input("Enter today's humidity (%):", format="%0.1f", help="Humidity percentage")
+    WindSpeed = st.number_input("Enter today's wind speed (km/h):", format="%0.1f", help="Wind speed in kilometers per hour")
+
+    # prediction
+    if st.button("Generate forecast"):
+        prediction = md.model.predict([[MinTemp, MaxTemp, Rainfall, Humidity, WindSpeed]])
+        if prediction == 1:
+            st.write("Yes, it might rain tomorrow.")
+        else:
+            st.write("No, it's not likely to rain tomorrow.")
