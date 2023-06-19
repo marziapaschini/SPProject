aabd03e5404800562291be5dadb8076dd69c0e91
marziapaschini <100375901+marziapaschini@users.noreply.github.com>
2023-06-19 18:21:15 +0200

    Useless import deleted

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import data_preprocessing as dp
 import pandas as pd
-import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
